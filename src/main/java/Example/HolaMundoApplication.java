c609d78d3d6707faa3ff6638c39d34ffb4c43c03
Verlaine99 <camilo.achury.mt@usa.edu.co>
2022-09-22 12:09:57 -0500

    Version 1.0

---
+++ src/main/java/Example/HolaMundoApplication.java
@@ -0,0 +1,13 @@
+package Example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HolaMundoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HolaMundoApplication.class, args);
+	}
+
+}
